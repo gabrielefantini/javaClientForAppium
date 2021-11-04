a3e0e72e2375b957f5d06a58cdc59ec103281431
gabrielefantini <gabrielefantini97@gmail.com>
2021-11-04 17:43:43 +0100

    Archive Note with swipe action

--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -4,6 +4,7 @@ import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
@@ -13,6 +14,8 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
 
 public class Tests {
 
@@ -50,7 +53,7 @@ public class Tests {
 
         AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
-        /*MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el1.click();
         MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el2.click();
@@ -59,42 +62,30 @@ public class Tests {
         el3.sendKeys("New Note");
         MobileElement el4 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
         el4.click();
-        *//*Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "New Note");*//*
 
-        MobileElement el5 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout"));
-        el5.click();
-        MobileElement el6 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("More options"));
+        int screenWidth = (int) driver.manage().window().getSize().width;
+        int screenWidth90 = (int) (.9 * screenWidth);
+        int screenWidth65 = (int) (.65 * screenWidth);
+        int screenWidth10 = (int) (.1 * screenWidth);
+
+        MobileElement newNote = driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title"));
+
+        TouchAction touchAction = new TouchAction(driver);
+        int elementY = newNote.getLocation().getY();
+
+        touchAction.press(PointOption.point(screenWidth90, elementY))
+                .moveTo(PointOption.point(screenWidth65, elementY))
+                .moveTo(PointOption.point(screenWidth10, elementY))
+                .release()
+                .perform();
+
+
+        MobileElement el6 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
         el6.click();
-        MobileElement el7 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[5]/android.widget.LinearLayout"));
+        MobileElement el7 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout"));
         el7.click();
-        MobileElement el8 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
-        el8.click();
-        MobileElement el9 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[5]/android.widget.LinearLayout"));
-        el9.click();*/
-        MobileElement el9 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
-        el9.click();
-        MobileElement el10 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
-        el10.click();
-        MobileElement el11 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
-        el11.click();
-        MobileElement el12 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
-        el12.sendKeys("New Note");
-        MobileElement el13 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
-        el13.click();
-        MobileElement el14 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout"));
-        el14.click();
-        MobileElement el15 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("More options"));
-        el15.click();
-        MobileElement el16 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[5]/android.widget.LinearLayout"));
-        el16.click();
-        MobileElement el17 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
-        el17.click();
-        MobileElement el18 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout"));
-        el18.click();
-
 
         Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "New Note");
-
     }
 
 }
