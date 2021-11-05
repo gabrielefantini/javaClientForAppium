8b822f820716c79af007fb64606849699aeb21d9
gabrielefantini <gabrielefantini97@gmail.com>
2021-11-05 12:56:53 +0100

    Added SearchNote test

--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -3,6 +3,8 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
@@ -88,5 +90,56 @@ public class Tests {
         Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "New Note");
     }
 
+    @Test
+    void searchNote()  throws MalformedURLException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5559");
+        dc.setCapability("platformName", "android");
+        dc.setCapability("appPackage", "it.feio.android.omninotes.alpha");
+        dc.setCapability("appActivity", "it.feio.android.omninotes.MainActivity");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+        MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        el1.click();
+        MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el3.click();
+        el3.sendKeys("X");
+        MobileElement el4 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        el4.click();
+        MobileElement el5 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        el5.click();
+        MobileElement el6 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        el6.click();
+        MobileElement el7 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el7.click();
+        MobileElement el8 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el8.sendKeys("Y");
+        MobileElement el9 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        el9.click();
+        MobileElement el10 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        el10.click();
+        MobileElement el11 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        el11.click();
+        MobileElement el12 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el12.click();
+        el12.sendKeys("Z");
+        MobileElement el13 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        el13.click();
+        MobileElement el14 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("Search"));
+        el14.click();
+        MobileElement el15 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/search_plate"));
+        el15.click();
+        MobileElement el16 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/search_src_text"));
+        el16.sendKeys("X");
+
+        ((AndroidDriver)driver).pressKey(new KeyEvent(AndroidKey.ENTER));
+
+        Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "X");
+
+
+    }
 }
 
