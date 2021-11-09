2f6c32d353e09c169264333696673c8955cc95e3
gabrielefantini <gabrielefantini97@gmail.com>
2021-11-09 16:23:00 +0100

    final commit

--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -18,8 +18,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
-import java.util.List;
+
 
 public class Tests {
 
@@ -32,18 +31,31 @@ public class Tests {
         dc.setCapability("appPackage", "it.feio.android.omninotes.alpha");
         dc.setCapability("appActivity", "it.feio.android.omninotes.MainActivity");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+        AndroidDriver<AndroidElement> driver =
+                new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
-        MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        MobileElement el1 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el1.click();
-        MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+
+        MobileElement el2 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el2.click();
-        MobileElement el3 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+
+        MobileElement el3 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el3.click();
         el3.sendKeys("New Note");
-        MobileElement el4 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+
+        MobileElement el4 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el4.click();
-        Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "New Note");
+
+        Assert.assertEquals(
+                driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title"))
+                        .getText(),
+                "New Note"
+        );
     }
 
     @Test
@@ -55,18 +67,26 @@ public class Tests {
         dc.setCapability("appPackage", "it.feio.android.omninotes.alpha");
         dc.setCapability("appActivity", "it.feio.android.omninotes.MainActivity");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+        AndroidDriver<AndroidElement> driver =
+                new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
-        MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        MobileElement el1 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el1.click();
-        MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+
+        MobileElement el2 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el2.click();
-        MobileElement el3 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+
+        MobileElement el3 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el3.click();
         el3.sendKeys("New Note");
+
         MobileElement el4 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
         el4.click();
 
+        //swipe left note to archive it
         int screenWidth = (int) driver.manage().window().getSize().width;
         int screenWidth90 = (int) (.9 * screenWidth);
         int screenWidth65 = (int) (.65 * screenWidth);
@@ -83,9 +103,10 @@ public class Tests {
                 .release()
                 .perform();
 
-
-        MobileElement el6 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        MobileElement el6 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el6.click();
+
         MobileElement el7 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout"));
         el7.click();
 
@@ -101,47 +122,68 @@ public class Tests {
         dc.setCapability("appPackage", "it.feio.android.omninotes.alpha");
         dc.setCapability("appActivity", "it.feio.android.omninotes.MainActivity");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
-        MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        AndroidDriver<AndroidElement> driver =
+                new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+
+        MobileElement el1 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el1.click();
-        MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        MobileElement el2 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el2.click();
-        MobileElement el3 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        MobileElement el3 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el3.click();
         el3.sendKeys("X");
-        MobileElement el4 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        MobileElement el4 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el4.click();
-        MobileElement el5 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        MobileElement el5 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el5.click();
-        MobileElement el6 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        MobileElement el6 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el6.click();
-        MobileElement el7 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        MobileElement el7 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el7.click();
-        MobileElement el8 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        MobileElement el8 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el8.sendKeys("Y");
-        MobileElement el9 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        MobileElement el9 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el9.click();
-        MobileElement el10 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        MobileElement el10 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el10.click();
-        MobileElement el11 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        MobileElement el11 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el11.click();
-        MobileElement el12 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        MobileElement el12 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el12.click();
         el12.sendKeys("Z");
-        MobileElement el13 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        MobileElement el13 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el13.click();
-        MobileElement el14 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("Search"));
+        MobileElement el14 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("Search"));
         el14.click();
-        MobileElement el15 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/search_plate"));
+        MobileElement el15 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/search_plate"));
         el15.click();
-        MobileElement el16 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/search_src_text"));
+        MobileElement el16 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/search_src_text"));
         el16.sendKeys("X");
 
         ((AndroidDriver)driver).pressKey(new KeyEvent(AndroidKey.ENTER));
 
-        Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "X");
-
-
+        Assert.assertEquals(
+                driver.findElement(
+                        By.id("it.feio.android.omninotes.alpha:id/note_title")
+                ).getText(),
+                "X"
+        );
     }
 
     @Test
@@ -153,34 +195,49 @@ public class Tests {
         dc.setCapability("appPackage", "it.feio.android.omninotes.alpha");
         dc.setCapability("appActivity", "it.feio.android.omninotes.MainActivity");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+        AndroidDriver<AndroidElement> driver =
+                new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        MobileElement el1 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        MobileElement el1 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
         el1.click();
-        MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+
+        MobileElement el2 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
         el2.click();
-        MobileElement el3 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+
+        MobileElement el3 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
         el3.click();
         el3.sendKeys("New Note");
-        MobileElement el4 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+
+        MobileElement el4 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el4.click();
-        MobileElement el5 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout"));
+
+        MobileElement el5 = (MobileElement) driver
+                .findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout"));
         el5.click();
+
         MobileElement el6 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("More options"));
         el6.click();
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[6]/android.widget.LinearLayout"))).click();
 
-        MobileElement el8 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        MobileElement el8 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el8.click();
-        MobileElement el9 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.TextView"));
+
+        MobileElement el9 = (MobileElement) driver
+                .findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.TextView"));
         el9.click();
 
         Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "New Note");
 
-        MobileElement el10 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("More options"));
+        MobileElement el10 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("More options"));
         el10.click();
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout"))).click();
@@ -203,13 +260,17 @@ public class Tests {
         dc.setCapability("appPackage", "it.feio.android.omninotes.alpha");
         dc.setCapability("appActivity", "it.feio.android.omninotes.MainActivity");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+        AndroidDriver<AndroidElement> driver =
+                new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        MobileElement el1 = (MobileElement) driver.findElement(MobileBy.AccessibilityId("drawer open"));
+        MobileElement el1 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
         el1.click();
-        MobileElement el2 = (MobileElement) driver.findElement(By.id("it.feio.android.omninotes.alpha:id/settings"));
+
+        MobileElement el2 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/settings"));
         el2.click();
 
         int screenH = (int) driver.manage().window().getSize().height;
@@ -222,10 +283,14 @@ public class Tests {
                 .release()
                 .perform();
 
-        MobileElement el3 = (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[13]"));
+        MobileElement el3 = (MobileElement) driver
+                .findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[13]"));
         el3.click();
 
-        Assert.assertEquals(driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView")).getText(), "Info");
+        Assert.assertEquals(
+                driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView")).getText(),
+                "Info"
+        );
     }
 }
 
